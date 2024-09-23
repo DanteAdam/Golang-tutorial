92c1ecafaacebe3eff1fa966b85ddd4a611c6c46
Nguyen Duy Tuan <tuannguyenduy2@gmail.com>
2024-09-24 00:14:20 +0700

    update

---
+++ pratice-project/prices/prices.go
@@ -0,0 +1,25 @@
+package prices
+
+import "fmt"
+
+type taxIncludedPricesJob struct {
+	TaxRate           float64
+	InputPrices       []float64
+	taxIncludedPrices map[string]float64
+}
+
+func (job taxIncludedPricesJob) Process() {
+	result := make(map[string]float64)
+
+	for _, price := range job.InputPrices {
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+	}
+	fmt.Println(result)
+}
+
+func NewTaxIncludedPriceJob(taxRate float64) *taxIncludedPricesJob {
+	return &taxIncludedPricesJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
