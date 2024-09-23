e5c0237c62913a7fa6d5e7a1ccdbe18a236aa715
Nguyen Duy Tuan <100367859+DanteAdam@users.noreply.github.com>
2024-09-23 16:20:30 +0700

    function deep dive

---
+++ functions-deep-dive/functions-as-value/functionsasvalue.go
@@ -0,0 +1,48 @@
+package functionasvalue
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformNumbers)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	dNumbers := []int{}
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
+	}
+	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
---
+++ functions-deep-dive/go.mod
@@ -0,0 +1,3 @@
+module deep_func
+
+go 1.22.2
