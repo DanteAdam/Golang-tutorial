9a8eac1cf171d3e81fcdbb269642cb5d5e7eacaa
Nguyen Duy Tuan <100367859+DanteAdam@users.noreply.github.com>
2024-09-24 15:43:07 +0700

    pratice project

--- pratice-project/prices/prices.go
+++ pratice-project/prices/prices.go
@@ -1,20 +1,49 @@
 package prices
 
-import "fmt"
+import (
+	"fmt"
+
+	"example.com/price-cal/conversion"
+	"example.com/price-cal/filemanager"
+)
 
 type taxIncludedPricesJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	taxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
-func (job taxIncludedPricesJob) Process() {
-	result := make(map[string]float64)
+func (job *taxIncludedPricesJob) LoadData() {
+
+	lines, err := filemanager.ReadLines("prices.txt")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	prices, err := conversion.StringsToFloats(lines)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	job.InputPrices = prices
+}
+
+func (job *taxIncludedPricesJob) Process() {
+	job.LoadData()
+
+	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		taxIncludedPrices := price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%2.f", taxIncludedPrices)
 	}
-	fmt.Println(result)
+
+	job.TaxIncludedPrices = result
+
+	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate*100), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *taxIncludedPricesJob {
